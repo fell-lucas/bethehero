caff2778779e09238aa756bc71c1fcf6da77094c
Lucas <lucasafell+github@gmail.com>
2020-03-25 03:02:17 -0300

    backend

---
+++ backend/src/routes.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const connection = require('./database/connection')
+const routes = express.Router()
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/ProfileController')
+const SessionController = require('./controllers/SessionController')
+
+routes.post('/session', SessionController.create)
+
+routes.get('/ongs', OngController.index)
+routes.post('/ongs', OngController.create)
+
+routes.get('/incidents', IncidentController.index)
+routes.post('/incidents', IncidentController.create)
+routes.delete('/incidents/:id', IncidentController.delete)
+
+routes.get('/profile', ProfileController.index)
+module.exports = routes
